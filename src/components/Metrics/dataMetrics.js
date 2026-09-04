b51214e844b9227dac72e58335bab467ea6e2c4a
d4lion <d4lion.dev@gmail.com>
2026-09-04 09:26:53 -0500

    refactor: extract data arrays to separate files

---
+++ src/components/Metrics/dataMetrics.js
@@ -0,0 +1,6 @@
+export const dmetrics = [
+  { value: 19, suffix: '+', label: 'Patrocinadores', sub: 'que brindaran una experiencia unica', color: '#9c3aed' },
+  { value: 15, suffix: '+', label: 'Espacios de aprendizaje', sub: 'por medio de talleres charlas T  y ponencias', color: '#4c29b6' },
+  { value: 20, suffix: '+', label: 'Ponentes', sub: 'expertos de la industria', color: '#828dbc' },
+  { value: 18, suffix: 'h', label: 'Horas', sub: 'de contenido y networking', color: '#c3abdc' },
+]
