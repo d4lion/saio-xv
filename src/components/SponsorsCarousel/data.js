0a04ba33d3254b318b7a9e9bf5028feb1d17f4c2
d4lion <d4lion.dev@gmail.com>
2026-09-13 23:28:36 -0500

    feat: add custom size support for sponsors marquee items and update sponsor assets

--- src/components/SponsorsCarousel/data.js
+++ src/components/SponsorsCarousel/data.js
@@ -2,11 +2,12 @@ import adamindLogo from '../../assets/sponsor/adamind.png';
 import colantaLogo from '../../assets/sponsor/colanta.png'
 import buguiLogo from '../../assets/sponsor/bugui.webp'
 import mounsieurWaffle from '../../assets/sponsor/monsieurWaffle.jpg'
-
+import logistica25 from '../../assets/sponsor/logistica25.png'
 
 export const sponsors = [
-  { id: 1, name: 'Adamind Technologies', logo: adamindLogo, src: 'https://www.adamind.cloud' },
-  { id: 2, name: 'Colanta', logo: colantaLogo, src: 'https://www.colanta.com' },
-  { id: 3, name: 'Bugui', logo: buguiLogo, src: 'https://heladosbugui.co/' },
-  { id: 4, name: 'Monsieur Waffle', logo: mounsieurWaffle, src: 'https://www.instagram.com/monsieurwaffle/' }
+  { id: 1, name: 'Adamind Technologies', logo: adamindLogo, src: 'https://www.adamind.cloud', size: 60 },
+  { id: 2, name: 'Colanta', logo: colantaLogo, src: 'https://www.colanta.com', size: 50 },
+  { id: 3, name: 'Bugui', logo: buguiLogo, src: 'https://heladosbugui.co/', size: 50 },
+  { id: 4, name: 'Monsieur Waffle', logo: mounsieurWaffle, src: 'https://www.instagram.com/monsieurwaffle/', size: 50 },
+  { id: 5, name: 'Logistica25', logo: logistica25, src: 'https://www.zonalogistica.com/', size: 40 }
 ]
